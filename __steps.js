177ae9f77d086726b7aa8c9865a1893865413eab
Rakib <rakib@Rakibs-MacBook-Pro.local>
2022-09-19 01:58:56 +0600

    connection to the server site

--- __steps.js
+++ __steps.js
@@ -15,3 +15,36 @@
  * 2.cors k require korbo
  * 2.app.use(cors())
 */
+
+/**
+ * Mongodb connection
+ * 1.create a node server with five steps
+ * 1.1.create a folder
+ * 1.2.npm init
+ * 1.3. npm i express cors mongodb
+ * 1.4.scripts-dev : nodemon index.js
+ * 1.5.create index.js
+ * 1.6.use 5 steps to create a node server
+ * 
+ * --------------------------
+ * create atlas account
+ * -----------------
+ * 1.sign up google access
+ * 2.create cluster
+ * 3.create user dbUser and password
+ * 4.Network access -->ip address :allow all
+ * 5.database >connect >code copy paste in index.js
+ * 
+ * -----------------------
+ * CRUD OPERATION
+ * 1.node mongodb crud search >fundatamentals
+ * 2.create async run function
+ *  
+ * --------------------
+ * Integrate sending data from client
+ * -----------------------------
+ * 1.client side  : create a form
+ * 2.onSubmit get form data and crate a object
+ * 3.on server : create user POST method to recieve  data on the backend
+ * 4.on client side :set fetch fetch with Post ,headers,body
+*/
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = process.env.PORT ||5000;
+const port = process.env.PORT ||4000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 
 //middleware
 app.use(cors());
@@ -11,6 +13,35 @@ app.get("/",(req,res)=>{
     res.send("running my node mongo crud server");
 
 })
+
+
+
+
+
+
+
+const uri = "mongodb+srv://dbUser:FSekc8HCslB70PsB@cluster0.rjgygoo.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+      await client.connect();
+      const usersCollection = client.db("foodExpress").collection("user");
+    
+      app.POST('/user',(req,res)=>{
+        const newUser = req.body;
+    
+        console.log("Adding new User" , newUser);
+        res.send({result : 'success'});
+    
+    });
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir);
+
 
 app.listen(port,()=>{
     console.log("crud server is running");
