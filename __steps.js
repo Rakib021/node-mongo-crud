90789ff8e8cd777637b5799573da4d03301d498b
Rakib <rakib@Rakibs-MacBook-Pro.local>
2022-09-18 12:12:41 +0600

    basic installation server

---
+++ __steps.js
@@ -0,0 +1,17 @@
+/**
+ * ----------
+ * basic installation
+ * 1.install express nodemon cors
+ * 2.express k require
+ * 3.app banabo express()
+ * 4.port dibo
+ * 6get korbo
+ * 7.losten korbo
+ * 
+ * 
+ * -------
+ * 8.add middleware for communicate client site and server site
+ * 1.install cors
+ * 2.cors k require korbo
+ * 2.app.use(cors())
+*/
